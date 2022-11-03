6e9cb7ce23c4569a256d180cabd57bf50cb8d608
vglumac <100020960+vglumac@users.noreply.github.com>
2022-11-03 17:56:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const screenSize = window.matchMedia("(min-width: 800px)");
 const heroImg = document.querySelector('.hero__image img');
 const navMenu = document.querySelector('.header__nav');
 const navToggle = document.querySelector('.nav__toggle-button');
-const subMenuToggles = document.querySelectorAll('.header__nav-sub-menu');
+const subMenuToggles = document.querySelectorAll('.subMenuClosed');
 const subMenus = document.querySelectorAll('.sub-menu');
 
 function handleScreenSizeChange(screenSize) {
@@ -13,13 +13,21 @@ function handleScreenSizeChange(screenSize) {
         navToggle.classList.add('hidden');
     } else {
         heroImg.src = './images/image-hero-mobile.png';
-        navMenu.classList.add('hidden');
         navToggle.classList.remove('hidden');
     }
 }
 
 function openMenu() {
     navMenu.classList.toggle('slideIn');
+    navMenu.classList.toggle('drop-shadow');
+    if(navMenu.classList.contains('slideIn')) {
+        navToggle.querySelector('img').src = './images/icon-close-menu.svg';
+        navToggle.style.position = 'fixed';
+    } else {
+        navToggle.querySelector('img').src = './images/icon-menu.svg';
+        navToggle.style.position = 'absolute';
+    }
+
 }
 
 function openSubMenu(index) {
@@ -31,7 +39,7 @@ handleScreenSizeChange(screenSize);
 screenSize.addEventListener('change', handleScreenSizeChange);
 navToggle.addEventListener('click', openMenu);
 subMenuToggles.forEach((subMenuToggle, index) => {
-    subMenuToggle.addEventListener('click', e => {
+    subMenuToggle.addEventListener('mouseover', e => {
         e.stopPropagation;
         openSubMenu(index);
     })
